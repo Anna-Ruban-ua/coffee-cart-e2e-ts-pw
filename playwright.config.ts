272db5aae54019e93959c4680e95f5f850070740
Anna Ruban <anna.ruban0902@gmail.com>
2026-09-21 02:02:20 +0300

    update Playwright configuration and add new test cases for coffee cart functionality using css selectors

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,6 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -26,27 +25,27 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://coffee-cart.app',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    testIdAttribute: 'data-test',
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'coffee-cart-css',
+      testDir: './tests/coffee-cart-css',
+      use: { 
+        ...devices['Desktop Chrome'] },
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: 'coffee-cart-e2e',
+      testDir: './tests/coffee-cart-e2e',
+      use: { 
+        ...devices['Desktop Chrome'] },
     },
 
     /* Test against mobile viewports. */
---
+++ tests/coffee-cart-css/coffee-cart-css.spec.ts
@@ -0,0 +1,59 @@
+import { test, expect } from '@playwright/test';
+
+test('Check cart reset after successful purchase', async ({ page }) => {
+  await page.goto('');
+  await expect(page.locator('[data-test="checkout"]')).toContainText('Total: $0.00');
+  await page.locator('[data-test="Espresso"]').click();
+  await expect(page.locator('[data-test="checkout"]')).toContainText('Total: $10.00');
+  await page.locator('[data-test="checkout"]').click();
+
+  await page.locator('input[name="name"]').fill('Anna');
+  await page.locator('input[name="email"]').fill('anna@gmail.com');
+  await page.locator('button[type="submit"]').click();
+  await expect(page.locator('[data-test="checkout"]')).toContainText('Total: $0.00');
+});
+
+test('Check all products is displayed', async ({ page }) => {
+  await page.goto('');
+  const products = await page.locator('li:has(h4)').allTextContents();
+  const expectedProducts = ["Espresso", "Espresso Macchiato", "Cappuccino", "Mocha", "Flat White", "Americano", "Cafe Latte", "Espresso Con Panna", "Cafe Breve"];
+
+  for (let i = 0; i < products.length; i++) {
+    expect(products[i]).toContain(expectedProducts[i]);
+  }
+});
+
+test('Check the extra cup proposal is displayed and matching the design', async ({ page }) => {
+  await page.goto('');
+  await page.locator('[data-test="Espresso"]').click();
+  await page.locator('[data-test="Flat_White"]').click();
+  await page.locator('[data-test="Cappuccino"]').click();
+  await expect(page.locator('.promo>span')).toContainText('It\'s your lucky day! Get an extra cup of Mocha for $4.');
+  await expect(page.locator('[data-test="(Discounted)_Mocha"]')).toBeVisible();
+  await expect(page.locator('button[class="yes"]')).toBeVisible();
+  await expect(page.locator('button[class="yes"]+button')).toBeVisible();
+});
+
+test('Check that user can accept extra cup proposal and total increases for 4$', async ({ page }) => {
+  await page.goto('');
+  await page.locator('[data-test="Cafe_Latte"]').click();
+  await page.locator('[data-test="Espresso_Con Panna"]').click();
+  await page.locator('[data-test="Americano"]').click();
+  await expect(page.locator('[data-test="checkout"]')).toContainText('Total: $37.00');
+  await expect(page.locator('.promo>span')).toBeVisible();
+  
+  await page.locator('button[class="yes"]').click();
+  await expect(page.locator('[data-test="checkout"]')).toContainText('Total: $41.00');
+});
+
+test('Check the user can add one more product in total product list and total increases x2', async ({ page }) => {
+  await page.goto('');
+  await page.locator('[data-test="Espresso_Macchiato"]').click();
+  await expect(page.locator('[data-test="checkout"]')).toContainText('Total: $12.00');
+  await page.locator('[data-test="checkout"]').hover();
+  await expect(page.locator('#app')).toContainText('Espresso Macchiato x 1+-');
+
+  await page.locator('button[aria-label="Add one Espresso Macchiato"]').click();
+  await expect(page.locator('#app')).toContainText('Espresso Macchiato x 2+-');
+  await expect(page.locator('[data-test="checkout"]')).toContainText('Total: $24.00');
+});
