829934554dce40892d92dbf47b6f2154cde6d9b8
Anna Ruban <anna.ruban0902@gmail.com>
2026-09-21 19:31:21 +0300

    refactor test locators to use more specific selectors for improved clarity and consistency

--- tests/coffee-cart-css/coffee-cart-css.spec.ts
+++ tests/coffee-cart-css/coffee-cart-css.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('Check cart reset after successful purchase', async ({ page }) => {
   await page.goto('');
   await expect(page.locator('button[class="pay"]')).toContainText('Total: $0.00');
-  await page.locator('[class="cup-body"][data-cy="Espresso"]').click();
+  await page.locator('li:has-text("Espresso $10.00") .cup-body').click();
   await expect(page.locator('button[class="pay"]')).toContainText('Total: $10.00');
   await page.locator('button[class="pay"]').click();
 
@@ -25,35 +25,35 @@ test('Check all products is displayed', async ({ page }) => {
 
 test('Check the extra cup proposal is displayed and matching the design', async ({ page }) => {
   await page.goto('');
-  await page.locator('[class="cup-body"][data-cy="Espresso"]').click();
-  await page.locator('[class="cup-body"][data-cy="Flat-White"]').click();
-  await page.locator('[class="cup-body"][data-cy="Espresso-Macchiato"]').click();
+  await page.locator('li:has-text("Espresso $10.00") .cup-body').click();
+  await page.locator('li:has-text("Flat White $18.00") .cup-body').click();
+  await page.locator('li:has-text("Espresso Macchiato $12.00") .cup-body').click();
   await expect(page.locator('.promo>span')).toContainText('It\'s your lucky day! Get an extra cup of Mocha for $4.');
-  await expect(page.locator('[class="cup-body disabled-hover"]')).toBeVisible();
+  await expect(page.locator('.promo .cup-body')).toBeVisible();
   await expect(page.locator('.promo button[class="yes"]')).toBeVisible();
   await expect(page.locator('.promo button:not(.yes)')).toBeVisible();
 });
 
 test('Check that user can accept extra cup proposal and total increases for 4$', async ({ page }) => {
   await page.goto('');
-  await page.locator('[class="cup-body"][data-cy="Cafe-Latte"]').click();
-  await page.locator('[class="cup-body"][data-cy="Espresso-Con Panna"]').click();
-  await page.locator('[class="cup-body"][data-cy="Americano"]').click();
+  await page.locator('li:has-text("Cafe Latte $16.00") .cup-body').click();
+  await page.locator('li:has-text("Espresso Con Panna $14.00") .cup-body').click();
+  await page.locator('li:has-text("Americano $7.00") .cup-body').click();
   await expect(page.locator('button[class="pay"]')).toContainText('Total: $37.00');
   await expect(page.locator('.promo>span')).toBeVisible();
   
-  await page.locator('button[class="yes"]').click();
+  await page.locator('.promo button[class="yes"]').click();
   await expect(page.locator('button[class="pay"]')).toContainText('Total: $41.00');
 });
 
 test('Check the user can add one more product in total product list and total increases x2', async ({ page }) => {
   await page.goto('');
-  await page.locator('[class="cup-body"][data-cy="Espresso-Macchiato"]').click();
+  await page.locator('li:has-text("Espresso Macchiato $12.00") .cup-body').click();
   await expect(page.locator('button[class="pay"]')).toContainText('Total: $12.00');
   await page.locator('button[class="pay"]').hover();
   await expect(page.locator('#app')).toContainText('Espresso Macchiato x 1+-');
 
-  await page.locator('button[aria-label="Add one Espresso Macchiato"]').click();
+  await page.locator('.unit-controller button:first-child').click();
   await expect(page.locator('#app')).toContainText('Espresso Macchiato x 2+-');
   await expect(page.locator('button[class="pay"]')).toContainText('Total: $24.00');
 });
